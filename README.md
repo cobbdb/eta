ac09e86afe9c60550d7074c2f037ab3d9ff95666
cobbdb <cobbdb@gmail.com>
2013-04-17 22:25:22 -0700

    Conversion to eta from old PEquals engine. Initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Eta
+### A lightweight and transparent templating engine designed for prototypes and small web applications.  
+Version **2.0.1**
+
+---
+The symbol for the Greek capital letter eta is H. Eta is often used in math as the symbol for efficiency.
+
+This engine was designed for prototypes and small-scale web applications that need to be developed and deployed quickly and with minimal setup. Since Eta no custom template tags, projects can be easily turned over to a more robust framework like Cake or Yii at a later date.
+
+### INSTALLATION
+* Copy this file into a directory of your choosing and add the require_once to the file that is using Eta.
+* The default directory for Eta is DocumentRoot/eta/ , but may be changed via <code>H::setHome()</code>.
+* The default page template for Eta is base.view , but this may be changed via <code>H::setBase()</code>.
+
+### USAGE
+Here is a simple hello world example:
+###### In your template
+```php
+Hello <?= $name ?>!
+```
+###### In your controller
+```php
+require_once("eta.php");
+$who = "World";
+echo H::render(null, [
+    "name" => $who
+]);
+```  
+And here is an example of a template include:
+###### In your template
+```php
+<div id="myWidget">
+    <? H::render("neatWidget.html") ?>
+</div>
+```  
+To set your default template directory:
+```php
+H::setHome("/path/to/my/templates");
+```  
+
+---
+By Dan Cobb: cobbdb@gmail.com - [petitgibier.sytes.net](http://petitgibier.sytes.net)
---
+++ eta.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * eta.php
+ * 
+ * Eta is a lightweight and transparent templating engine designed for prototypes and small web applications.
+ * The symbol for the Greek capital letter eta is H. Eta is often used in math as the symbol for efficiency.
+ * 
+ * ~ INSTALLATION ~
+ * Copy this file into a directory of your choosing and add the require_once to the file that is using Eta.
+ * The default directory for Eta is DocumentRoot/eta/ , but may be changed via H::setHome().
+ * The default page template for Eta is base.view , but this may be changed via H::setBase().
+ * 
+ * @usage
+ * echo H::render(null, [
+ *     "someData" => $myData
+ * ]);
+ * 
+ * @author Dan Cobb
+ * @see http://proccli.com/2010/03/dead-simple-php-template-rendering/
+ * @see //www.github.com/cobbdb/eta
+ * @since 2.0.0
+ */
+
+class MissingTemplateException extends Exception {
+}
+
+final class H {
+    /**
+     * Establish the default views directory.
+     */
+    private static $home = $_SERVER["DOCUMENT_ROOT"] . "/eta/";
+    /**
+     * Establish the default base page template.
+     */
+    private static $base = "base.view";
+    
+    /**
+     * Set a new default views directory.
+     * @param {String} path
+     */
+    public static function setHome($path) {
+        self::$home = $path;
+    }
+    
+    /**
+     * Set a new default base page template.
+     * @param {String} path
+     */
+    public static function setBase($path) {
+        self::$base = $path;
+    }
+    
+    /**
+     * Render a template without auto-responding.
+     * @param {String} [path] Filename of the template to render.
+     * @param {Array} [model] Data to inject into the template.
+     * @param {Boolean} [grounded] False to use a literal path and bypass the home directory.
+     * @returns {String}
+     */
+    public static function render($path, $model = Array(), $grounded = true) {
+        if (!isset($path)) {
+            $path = self::$home . self::$base;
+        } else if ($grounded) {
+            $path = self::$home . $path;
+        }
+        
+        if (file_exists($path)) {
+            ob_start();
+            extract($model);
+            @include $path;
+            return ob_get_clean();
+        } else {
+            throw new MissingTemplateException("Template: $path could not be found!");
+        }
+    }
+}
